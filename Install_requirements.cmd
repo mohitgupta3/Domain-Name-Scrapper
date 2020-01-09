893a700200f2c84ced2a5d417e8227c8c9e752fa
Mohit Gupta <mohit.gupta2jan@gmail.com>
2020-01-09 12:29:15 +0530

    Add files via upload

---
+++ Install_requirements.cmd
@@ -0,0 +1,12 @@
+echo off
+color a
+cls
+pip install --upgrade urllib3
+pip install --upgrade requests
+pip install --upgrade validators
+pip install --upgrade beautifulsoup4
+pip install --upgrade collections
+pip install --upgrade datetime
+pip install --upgrade regex
+pip install --upgrade tld
+pip install --upgrade secrets
---
+++ RUN.cmd
@@ -0,0 +1,7 @@
+echo off
+color a
+title Compiling script...
+cls
+echo Compiling script!
+python domain.py
+PAUSE
